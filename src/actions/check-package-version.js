3f2ead8eabbae0a374f6256dd5cf2a266e001eba
Chandra Shekar Chennamsetty <chandra.shekar.chennamsetty@autodesk.com>
2019-08-05 15:52:52 -0700

    fix(CheckPackageVersion): Fixed cognitive complexity reported in codeclimate

--- src/actions/check-package-version.js
+++ src/actions/check-package-version.js
@@ -4,22 +4,22 @@ const packageJson = require('../../package.json');
 const uri = "https://registry.npmjs.org/@chandu1310/nodeneeds"
 
 export const checkPackageVersion = () => download(uri)
-.then(
-  response => {
-    let data;
-    if(response)
-      data = JSON.parse(response.toString())
-    console.log(`Nodeneeds ${packageJson.version}`);
-    console.log(`==================`);
-    console.log(`Checking for any new update.`);
-    if(data && data['dist-tags'] && data['dist-tags'].latest){
-      const latestVersion = data['dist-tags'].latest;
-      if(latestVersion !== packageJson.version){
-        console.log(`\nWarning!\nA new version ${latestVersion} is available.\nUpdate using the command:\n=>> npm update -g @chandu1310/nodeneeds\nor\n=>> yarn global upgrade @chandu1310/nodeneeds\n`);
-      }else{
-        console.log(`Installed version is the latest.`);
+  .then(
+    response => {
+      let data;
+      if (response)
+        data = JSON.parse(response.toString())
+      console.log(`Nodeneeds ${packageJson.version}`);
+      console.log(`==================`);
+      console.log(`Checking for any new update.`);
+      if (data && data['dist-tags'] && data['dist-tags'].latest) {
+        const latestVersion = data['dist-tags'].latest;
+        if (latestVersion !== packageJson.version) {
+          console.log(`\nWarning!\nA new version ${latestVersion} is available.\nUpdate using the command:\n=>> npm update -g @chandu1310/nodeneeds\nor\n=>> yarn global upgrade @chandu1310/nodeneeds\n`);
+        } else {
+          console.log(`Installed version is the latest.`);
+        }
       }
+      console.log(`Lets Begin!`);
     }
-    console.log(`Lets Begin!`);
-  }
-);
+  );
